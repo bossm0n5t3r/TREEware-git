585364daa074ead4e5a7b9b3215813efeb25b72d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-10-18 21:11:31 +0900

    basic setting to use schedule table - NOT CHECKED!

--- src/main/java/com/treeware/schedule/service/ScheduleServiceImpl.java
+++ src/main/java/com/treeware/schedule/service/ScheduleServiceImpl.java
@@ -1,9 +1,12 @@
 package com.treeware.schedule.service;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.treeware.admin.member.model.EmployeeDto;
 import com.treeware.admin.schedule.model.ScheduleDto;
 import com.treeware.schedule.dao.ScheduleDao;
 
@@ -18,6 +21,21 @@ public class ScheduleServiceImpl implements ScheduleService {
 		int scd_sq = sqlSession.getMapper(ScheduleDao.class).getNextScheduleSq();
 		scheduleDto.setScd_sq(scd_sq);
 		return sqlSession.getMapper(ScheduleDao.class).registerSchedule(scheduleDto);
+	}
+
+	@Override
+	public int modifySchedule(ScheduleDto scheduleDto) {
+		return sqlSession.getMapper(ScheduleDao.class).modifySchedule(scheduleDto);
+	}
+
+	@Override
+	public int deleteSchedule(int scd_sq) {
+		return sqlSession.getMapper(ScheduleDao.class).deleteSchedule(scd_sq);
+	}
+
+	@Override
+	public List<ScheduleDto> getListSchedule(EmployeeDto employeeDto) {
+		return sqlSession.getMapper(ScheduleDao.class).getListSchedule(employeeDto);
 	}
 
 }
