7cf82372b454e569f9bea1aa5aa089a5fb03a9e6
pymc20 <pymc20@naver.com>
2018-10-19 17:56:01 +0900

    board modify

---
+++ src/main/java/com/treeware/admin/board/model/BoardDto.java
@@ -0,0 +1,16 @@
+package com.treeware.admin.board.model;
+
+public class BoardDto {
+
+	private int brd_sq;
+	private int brd_div_sq;
+	private int ccode;
+	private String brd_ttl;
+	private String emp_sq;
+	private String brd_ctt;
+	private String brd_dt;
+	private int brd_hits;
+	private String brd_fl_onm;
+	private String brd_fl_mnm;
+	private String brd_fl_rt;
+}
