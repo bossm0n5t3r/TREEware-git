6fcf622c5a6d42f0be07621ee651d012207c2207
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-10-19 09:42:35 +0900

    Delete useless methods

--- src/main/java/com/treeware/member/service/MemberService.java
+++ src/main/java/com/treeware/member/service/MemberService.java
@@ -1,6 +1,5 @@
 package com.treeware.member.service;
 
-import java.util.List;
 import java.util.Map;
 
 import com.treeware.admin.member.model.EmployeeDto;
@@ -10,8 +9,4 @@ public interface MemberService {
 	// 로그인
 	public EmployeeDto login(Map<String, String> map);
 	
-	public List<Map> getDptCommute();
-//	public FormDto getVacation();
-	public String getCertificate();
-// 자율출퇴근
 }
