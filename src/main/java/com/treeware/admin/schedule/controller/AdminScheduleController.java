f0935251bba824a7756781a3bb4661da829d28cb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-10-21 19:27:59 +0900

    Add a new schedule, and Show all schedules in calendar with different colors

--- src/main/java/com/treeware/admin/schedule/controller/AdminScheduleController.java
+++ src/main/java/com/treeware/admin/schedule/controller/AdminScheduleController.java
@@ -2,6 +2,8 @@ package com.treeware.admin.schedule.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.treeware.admin.member.model.EmployeeDto;
 import com.treeware.admin.schedule.model.ScheduleDivideDto;
+import com.treeware.admin.schedule.model.ScheduleDto;
 import com.treeware.admin.schedule.service.AdminScheduleService;
 
 @Controller
@@ -41,6 +45,33 @@ public class AdminScheduleController {
 			array.put(scdDivDto);
 		}
 		object.put("scheduleDivList", array);
+		return object.toString();
+	}
+	
+	// 일정 가져오기
+	@RequestMapping(value="/getList.tree", method=RequestMethod.GET)
+	public @ResponseBody String getList(HttpSession session) {
+		JSONObject object = new JSONObject();
+		JSONArray array = new JSONArray();
+		EmployeeDto employeeDto = (EmployeeDto) session.getAttribute("userInfo");
+		List<ScheduleDto> list = adminScheduleService.getListSchedule(employeeDto.getEmp_sq());
+		for (ScheduleDto dto : list) {
+			JSONObject scheduleDto = new JSONObject();
+			scheduleDto.put("scd_sq", dto.getScd_sq());
+			scheduleDto.put("scd_div_sq", dto.getScd_div_sq());
+			scheduleDto.put("scd_div_nm", dto.getScd_div_nm());
+			scheduleDto.put("emp_sq", dto.getEmp_sq());
+			scheduleDto.put("scd_nm", dto.getScd_nm());
+			scheduleDto.put("scd_pst", dto.getScd_pst());
+			scheduleDto.put("scd_sday", dto.getScd_sday());
+			scheduleDto.put("scd_stime", dto.getScd_stime());
+			scheduleDto.put("scd_eday", dto.getScd_eday());
+			scheduleDto.put("scd_etime", dto.getScd_etime());
+			scheduleDto.put("scd_dct", dto.getScd_dct());
+			
+			array.put(scheduleDto);
+		}
+		object.put("scheduleList", array);
 		return object.toString();
 	}
 }
--- src/main/java/com/treeware/schedule/controller/ScheduleController.java
+++ src/main/java/com/treeware/schedule/controller/ScheduleController.java
@@ -1,16 +1,10 @@
 package com.treeware.schedule.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import javax.servlet.http.HttpSession;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.treeware.admin.member.model.EmployeeDto;
 import com.treeware.admin.schedule.model.ScheduleDto;
@@ -35,44 +29,13 @@ public class ScheduleController {
 		return "member/calendar/todo";
 	}
 	
-	// 일정 가져오기
-	@RequestMapping(value="/getList.tree", method=RequestMethod.GET)
-	public @ResponseBody String getList(HttpSession session) {
-		JSONObject object = new JSONObject();
-		JSONArray array = new JSONArray();
-		EmployeeDto employeeDto = (EmployeeDto) session.getAttribute("userInfo");
-		List<ScheduleDto> list = scheduleService.getListSchedule(employeeDto);
-		for (ScheduleDto dto : list) {
-			JSONObject scheduleDto = new JSONObject();
-			scheduleDto.put("scd_sq", dto.getScd_sq());
-			scheduleDto.put("emp_sq", dto.getEmp_sq());
-			scheduleDto.put("scd_nm", dto.getScd_nm());
-			scheduleDto.put("scd_pst", dto.getScd_pst());
-			scheduleDto.put("scd_sday", dto.getScd_sday());
-			scheduleDto.put("scd_stime", dto.getScd_stime());
-			scheduleDto.put("scd_eday", dto.getScd_eday());
-			scheduleDto.put("scd_etime", dto.getScd_etime());
-			scheduleDto.put("scd_dct", dto.getScd_dct());
-			
-			array.put(scheduleDto);
-		}
-		object.put("scheduleList", array);
-		return object.toString();
-	}
-	
 	// 일정 등록
 	@RequestMapping(value="/register.tree", method=RequestMethod.POST)
-	public ModelAndView register(ScheduleDto scheduleDto, @RequestParam Map<String, String> map, HttpSession session) {
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("member/calendar/main");
+	public String register(ScheduleDto scheduleDto, HttpSession session) {
 		EmployeeDto employeeDto = (EmployeeDto) session.getAttribute("userInfo");
-		String emp_sq = employeeDto.getEmp_sq();
-		scheduleDto.setEmp_sq(emp_sq);
-		int cnt = scheduleService.registerSchedule(scheduleDto);
-		if (cnt != 0) {
-			System.out.println("들어갔으니까 확인해봐");
-		}
-		return mav;
+		scheduleDto.setEmp_sq(employeeDto.getEmp_sq());
+		scheduleService.registerSchedule(scheduleDto);
+		return "member/calendar/main";
 	}
 
 }
