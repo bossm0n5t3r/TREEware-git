dac4c2a144824dbf9a19adec33db9647c76d2394
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-10-26 05:15:26 +0900

    Apply tree map for employees

--- src/main/webapp/assets/js/plugin/jquery-ztree/dept_ztree.js
+++ src/main/webapp/assets/js/plugin/jquery-ztree/dept_ztree.js
@@ -31,70 +31,75 @@
 	};
 
 	var zNodes =[
-		{ name:"대표이사",
-			children: [
-				{ name:"경영자문위원단",
-					children: [
-						{ name:"leaf node 111"},
-						{ name:"leaf node 112"},
-						{ name:"leaf node 113"},
-						{ name:"leaf node 114"}
-					]},
-				{ name:"이사회",
-					children: [
-						{ name:"leaf node 121"},
-						{ name:"leaf node 122"},
-						{ name:"leaf node 123"},
-						{ name:"leaf node 124"}
-					]},
-				{ name:"경영자문위원단", isParent:true}
-			]},
-		{ name:"영업본부",
-			children: [
-				{ name:"영업1팀",
-					children: [
-						{ name:"부장 김고은"},
-						{ name:"부장 김고은"},
-						{ name:"부장 김고은"},
-						{ name:"부장 김고은"}
-					]},
-				{ name:"영업2팀",
-					children: [
-						{ name:"부장 박윤민"},
-						{ name:"부장 박윤민"},
-						{ name:"부장 박윤민"},
-						{ name:"부장 박윤민"}
-					]},
-				{ name:"외식사업부",
-					children: [
-						{ name:"부장 강혜인"},
-						{ name:"부장 강혜인"},
-						{ name:"부장 강혜인"},
-						{ name:"부장 강혜인"}
-					]}
-			]},
-		{ name:"연구개발본부", open:true,
+		{ name:"트리웨어", open:true,
 				children: [
-					{ name:"시스템개발팀", open:true,
+					{ name:"관리부", open:true,
 						children: [
-							{ name:"부장 김고은"},
-							{ name:"대리 김고은"},
-							{ name:"사원 김고은"},
-							{ name:"인턴 김고은"}
-						]},
-					{ name:"개발기획팀", open:true,
-						children: [
-							{ name:"부장 김지훈"},
-							{ name:"대리 김지훈"},
+							{ name:"사장 김소현"},
+							{ name:"사장 유준"},
+							{ name:"사장 한영선"},
+							{ name:"차장 김재현"},
+							{ name:"차장 유태홍"},
+							{ name:"차장 손지인"},
+							{ name:"과장 김보민"},
+							{ name:"대리 안효일"},
+							{ name:"사원 강혜인"},
+							{ name:"사원 이형민"},
 							{ name:"사원 김지훈"},
-							{ name:"사원 김지훈"}
+							{ name:"사원 손영재"}
 						]},
-					{ name:"Task Force팀", open:true,
+					{ name:"인사과",
 						children: [
-							{ name:"부장 강수민"},
-							{ name:"대리 강수민"},
-							{ name:"사원 강수민"},
-							{ name:"사원 강수민"}
+							{ name:"사장 손주희"},
+							{ name:"부장 이수영"},
+							{ name:"부장 고운선"},
+							{ name:"차장 최홍석"},
+							{ name:"과장 한상현"},
+							{ name:"과장 강호성"},
+							{ name:"사원 김고은"},
+							{ name:"사원 정하영"},
+							{ name:"사원 서계범"}
+						]},
+					{ name:"무역부",
+						children: [
+							{ name:"부장 박정이"},
+							{ name:"부장 정홍균"},
+							{ name:"부장 이승하"},
+							{ name:"부장 송현태"},
+							{ name:"차장 임승은"},
+							{ name:"차장 강수민"},
+							{ name:"차장 오승경"},
+							{ name:"차장 백창렬"},
+							{ name:"차장 강수민"},
+							{ name:"과장 유하늘"},
+							{ name:"과장 강혜인"},
+							{ name:"대리 서도현"},
+							{ name:"사원 황규준"}
+						]},
+					{ name:"영업부",
+						children: [
+							{ name:"사장 고민주"},
+							{ name:"사장 김서영"},
+							{ name:"사장 신하은"},
+							{ name:"부장 이다인"},
+							{ name:"부장 양지현"},
+							{ name:"차장 김다영"},
+							{ name:"차장 김소이"},
+							{ name:"차장 이시형"},
+							{ name:"차장 고정열"},
+							{ name:"과장 김태리"},
+							{ name:"과장 강성욱"},
+							{ name:"대리 임자연"}
+						]},
+					{ name:"판매부",
+						children: [
+							{ name:"부장 김성진"},
+							{ name:"부장 박윤민"},
+							{ name:"부장 황수빈"},
+							{ name:"과장 강성민"},
+							{ name:"과장 최슬비"},
+							{ name:"대리 신영준"},
+							{ name:"사원 고동엽"}
 						]}
 				]}
 
