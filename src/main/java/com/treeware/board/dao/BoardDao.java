9b36eabe1bce9ceeddc07bc023c7ed4147b3cac3
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-11-18 07:00:36 +0900

    Rearrange methods

--- src/main/java/com/treeware/board/dao/BoardDao.java
+++ src/main/java/com/treeware/board/dao/BoardDao.java
@@ -1,9 +1,14 @@
 package com.treeware.board.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import com.treeware.admin.board.model.BoardDto;
 
 public interface BoardDao {
 	
 	public int write(BoardDto boardDto);
+	public String getBoardName(Map<String, String> map);
+	public List<BoardDto> listArticle(Map<String, String> map);
 	
 }
