4f687d2d05dd9f17e7a4ad02215a17af2ec3f65e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-11-18 05:56:50 +0900

    Add BoardDetailDto

---
+++ src/main/java/com/treeware/admin/board/model/BoardDetailDto.java
@@ -0,0 +1,14 @@
+package com.treeware.admin.board.model;
+
+public class BoardDetailDto extends BoardDto {
+	private String emp_nm;
+
+	public String getEmp_nm() {
+		return emp_nm;
+	}
+
+	public void setEmp_nm(String emp_nm) {
+		this.emp_nm = emp_nm;
+	}
+	
+}
