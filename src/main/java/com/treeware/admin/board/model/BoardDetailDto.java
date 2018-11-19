7ab7498c423a85562dad79f1c7cb4203f7bc084b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-11-20 02:35:35 +0900

    Add bname field

--- src/main/java/com/treeware/admin/board/model/BoardDetailDto.java
+++ src/main/java/com/treeware/admin/board/model/BoardDetailDto.java
@@ -2,6 +2,7 @@ package com.treeware.admin.board.model;
 
 public class BoardDetailDto extends BoardDto {
 	private String emp_nm;
+	private String bname;
 
 	public String getEmp_nm() {
 		return emp_nm;
@@ -10,5 +11,13 @@ public class BoardDetailDto extends BoardDto {
 	public void setEmp_nm(String emp_nm) {
 		this.emp_nm = emp_nm;
 	}
-	
+
+	public String getBname() {
+		return bname;
+	}
+
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+
 }
