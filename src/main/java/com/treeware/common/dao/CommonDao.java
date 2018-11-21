4464cff9eefb1d13292a13dc8f5985c320c55874
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-11-22 03:33:59 +0900

    Add method to get nextval of RPL seq

--- src/main/java/com/treeware/common/dao/CommonDao.java
+++ src/main/java/com/treeware/common/dao/CommonDao.java
@@ -5,6 +5,7 @@ import java.util.Map;
 public interface CommonDao {
 	
 	public int getNextBrdSeq();
+	public int getNextRplSeq();
 	public int getNewArticleCount(int bcode);
 	public int getTotalArticleCount(Map<String, String> map);
 	
